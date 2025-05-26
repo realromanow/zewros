b43cac9e1c184e940c63bca07ca0c5a15af03cee
Nikita Romanov <romanow.n.e@icloud.com>
2025-05-26 22:42:26 +0300

    add contexts

--- Assets/Core/App/SlotsGameViewBuilderService.cs
+++ Assets/Core/App/SlotsGameViewBuilderService.cs
@@ -45,14 +45,18 @@ namespace Core.App {
 
 			for (var i = 0; i < symbolsPacks.Length; i++) {
 				for (var j = 0; j < symbolsPacks[i].packLength; j++) {
-					if (symbolsPacks[i].packGeneration >= symbolsPacks[i].symbols[j].generation) continue;
+					if (symbolsPacks[i].packGeneration < symbolsPacks[i].symbols[j].generation) {
+						var viewModel = _symbolsViewModelsFactory.CreateViewModel(symbolsPacks[i].symbols[j], i, i * j, symbolsPacks[i].packLength, symbolsPacks.Length * symbolsPacks[i].packLength, contextComponent.columns[i].joints[j]);
+						viewModel.AddTo(disposables);
+						expireViews.Subscribe(_ => viewModel.Expire())
+							.AddTo(disposables);
 
-					var viewModel = _symbolsViewModelsFactory.CreateViewModel(symbolsPacks[i].symbols[j], i, i * j, symbolsPacks[i].packLength, symbolsPacks.Length * symbolsPacks[i].packLength, contextComponent.columns[i].joints[j]);
-					viewModel.AddTo(disposables);
-					expireViews.Subscribe(_ => viewModel.Expire())
-						.AddTo(disposables);
-
-					updatedSymbols.Add(viewModel);
+						updatedSymbols.Add(viewModel);
+					}
+					else {
+						var symbolPackId = symbolsPacks[i].symbols[j].id;
+						// вот здесь нужно найти элемент из _lastViewModels с таким же id
+					}
 				}
 			}
 			
